771b513a5c346e3cb3a3b580855c38becbdac9f3
PrabhasVarma60 <153572872+PrabhasVarma60@users.noreply.github.com>
2025-09-13 11:36:29 +0530

    Update urls.py

--- webapp_django/urls.py
+++ webapp_django/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
-    path('run-profile-fix-script-once/', user_views.temp_create_profiles, name='temp_create_profiles'),
+
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
