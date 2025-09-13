4f347416d5fc02c642bf77477ea9ab97084a2606
PrabhasVarma60 <153572872+PrabhasVarma60@users.noreply.github.com>
2025-09-13 10:53:05 +0530

    Update apps.py

--- users/apps.py
+++ users/apps.py
@@ -1,8 +1,20 @@
 from django.apps import AppConfig
 
 
+
+
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
     def ready(self):
         import users.signals
+         # Create admin if it doesn't exist
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username='prabhasvarma').exists():
+            User.objects.create_superuser(
+                'prabhasvarma',
+                'prabhasvarma6061@gmail.com',
+                'tixxy007'
+            )
+    
+       
